2a0abe61465d35fcebf1ad2c9f79dff82ad2dda5
Jeffersonddos <jeffersonnarley5555@gmail.com>
2024-09-16 09:20:16 -0300

    node_modules

---
+++ node_modules/gpt4js/src/Providers/ChatCompletion/provider.js
@@ -0,0 +1,8 @@
+"use strict";
+class Provider {
+  async chatCompletion(messages, options) {
+    throw new Error("Method 'chatCompletion()' must be implemented.");
+  }
+}
+
+export default Provider;
